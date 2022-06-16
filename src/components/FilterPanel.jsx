80aa100c7a361196c13161089342e84a7cd5ed30
deysan <ideysan@gmail.com>
2022-06-16 21:58:04 +0300

    Add remove filters and cleanup

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,22 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Badge } from 'UI/Badge';
 import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
 
+import { selectFilters } from 'store/filters/filter-selector';
+import { clearFilter, removeFilter } from 'store/filters/filter-actions';
 
 const FilterPanel = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+
+  const handleRemoveFilter = (filter) => {
+    dispatch(removeFilter(filter));
+  };
+
+  const handleClearFilters = () => {
+    dispatch(clearFilter);
+  };
+
+  if (currentFilters.length === 0) return null;
+
   return (
     <Card className="filter-panel">
       <div className="filter-panel-wrapper">
         <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
+          {currentFilters.map((filter) => (
+            <Badge
+              key={filter}
+              variant="clearable"
+              onClear={() => handleRemoveFilter(filter)}
+            >
+              {filter}
+            </Badge>
+          ))}
         </Stack>
 
-        <button className='link'>Clear</button>
+        <button className="link" onClick={() => handleClearFilters()}>
+          Clear
+        </button>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export {FilterPanel};
+export { FilterPanel };
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,12 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { JobPosition } from './JobPosition';
-import { selectAllPositions } from 'store/positions/position-selector';
+import { selectVisiblePositions } from 'store/positions/position-selector';
 
 import { addFilter } from 'store/filters/filter-actions';
+import { selectFilters } from 'store/filters/filter-selector';
 
 const JobList = () => {
-  const positions = useSelector(selectAllPositions);
   const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFilters)
+  );
 
   const handleAddFilter = (filter) => {
     dispatch(addFilter(filter));

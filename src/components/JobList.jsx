87ee1208e6fb27feb5779ac046b7f6269f25ddaa
deysan <ideysan@gmail.com>
2022-06-16 21:40:38 +0300

    Add ability to select filters

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { JobPosition } from './JobPosition';
 import { selectAllPositions } from 'store/positions/position-selector';
 
+import { addFilter } from 'store/filters/filter-actions';
+
 const JobList = () => {
   const positions = useSelector(selectAllPositions);
+  const dispatch = useDispatch();
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
 
   return (
     <div className="job-list">
       {positions.map((item) => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
   );
